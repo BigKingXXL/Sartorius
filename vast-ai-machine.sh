b6f4ed5f38afc3bcf414613925739ba9450319a5
Maximilian Schulze <49536186+max-3l@users.noreply.github.com>
2021-12-20 20:22:53 +0100

    add setup machine script

---
+++ vast-ai-machine.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+apt update
+apt install git unzip gcc nano ffmpeg libsm6 libxext6 g++ -y
+pip install pytorch-lightning pandas scipy scikit-learn scikit-image tiffile kaggle  opencv-python
+#mkdir ~/.kaggle -p
+#nano ~/.kaggle/kaggle.json
+git clone https://max-3l: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@github.com/BigKingXXL/Sartorius.git
+cd Sartorius
+git checkout pytorch-lightning
+/bin/bash ./get-data.sh
+python3 preprocess-dataset.py
+pip install openmim
+mim install mmdet
+git clone https://github.com/open-mmlab/mmdetection.git
+pip install instaboostfast git+https://github.com/cocodataset/panopticapi.git git+https://github.com/lvis-dataset/lvis-api.git
+pip install albumentations>=0.3.2 --no-binary imgaug,albumentations
